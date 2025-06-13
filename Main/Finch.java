b3d287f73845771449eb2b106029a8080381aa9c
Yi Feng Zhang <yifengz246@gmail.com>
2025-06-13 08:23:39 -0400

    Update and rename finch.java to Finch.java

---
+++ Main/Finch.java
@@ -0,0 +1,37 @@
+public class Finch {
+    private boolean isMoving = false;
+
+    public void setMove(String direction, int distance, int speed) {
+        isMoving = true;
+        System.out.println("Moving " + direction + " for " + distance + " units at speed " + speed);
+    }
+
+    public void setMove(String direction, double distance, double speed) {
+        isMoving = true;
+        System.out.println("Moving " + direction + " for " + distance + " units at speed " + speed);
+    }
+
+    public void stop() {
+        isMoving = false;
+        System.out.println("Robot stopped.");
+    }
+
+    public double getDistance() {
+        // Simulates a decreasing distance every call to mimic approaching an object
+        return Math.random() * 10; // Returns a number between 0 and 10
+    }
+
+    public String getCompass() {
+        // Returns a fixed direction (could randomize if needed)
+        return "North";
+    }
+
+    public boolean isShaking() {
+        // 30% chance of shaking
+        return Math.random() < 0.3;
+    }
+
+    public void setTurn(String direction, int angle, int speed) {
+        System.out.println("Turning " + direction + " by " + angle + " degrees at speed " + speed);
+    }
+}
